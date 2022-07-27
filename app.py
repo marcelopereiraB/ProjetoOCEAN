8d08c510d4094ac5b1f7eb017cafe697a7abe4b0
Marcelo Pereira <marcelopereiracelo0@gmail.com>
2022-07-27 21:12:23 +0000

    progresso

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
-
-@app.route("/") #rotas 
-@app.route("/hello") #rotas 
-def hello():
-    return "Hello word carai"
-
-@app.route("/meucontato")
-def meuContato():
-    return render_template('indexcontato.html')
-
+posts = [
+    {"" 
+           "title": "o meu post",
+            "body": "aqui é texto do post",
+            "autor": "celonerda",
+            "created": datetime(2022,7,25)
+    },
+    {
+            "title": "o meu post",
+            "body": "aqui é texto do post",
+            "autor": "sest",
+            "created": datetime(2022,7,26)
+    },
+    {"title": "o meu post",
+            "body": "aqui é texto do post",
+            "autor": "marcelo silva",
+            "created": datetime(2022,7,27)
+    },
+    {
+            "title": "o meu post",
+            "body": "aqui é texto do post",
+            "autor": "fernando silva",
+             "created": datetime(2022,7,28)
+    }
+        
+]
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
