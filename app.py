483d0a0e9b0debff822d81575694689002700e6f
Marcelo Pereira <marcelopereiracelo0@gmail.com>
2022-07-29 00:57:41 +0000

    um dia pro fim

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.exc import IntegrityError
+
 
 app = Flask("hello")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "marvel"
 
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
 class Post(db.Model):
         __tablename__ = 'posts'
@@ -16,14 +22,23 @@ class Post(db.Model):
         created = db.Column(db.DateTime, nullable=False, default=datetime.now)
         user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
         __tablename__ = "users"
         id = db.Column(db.Integer, primary_key=True, autoincrement=True)
         username = db.Column(db.String(25), nullable=False, unique=True, index=True )
         email = db.Column(db.String(64), nullable=False, unique=True)
-        password_hash = db.Column(db.String(128),nullable=False)
+        password_hash = db.Column(db.String(128), nullable=False)
         posts = db.relationship('Post', backref='autor')
 
+        def set_password(self, password):
+                self.password_hash = generate_password_hash(password)
+        
+        def creck_password(self, password):
+                return creck_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+        return User.query.get(int(id))
 
 db.create_all()
 
@@ -31,14 +46,44 @@ db.create_all()
 def index():
         posts = Post.query.all() 
         return render_template("index.html", posts=posts)
+      
+@app.route('/register', methods=["GET", "POST"])
+def register():
+        if current_user.is_authenticated:
+                return redirect(url_for('index'))
+        if request.method == "POST":
+                username = request.form['username']
+                email = request.form['email']
+                password = request.form['password']
+                try:
+                        new_user = User(username=username, email=email)
+                        new_user.set_password(password)
+                        db.session.add(new_user)
+                        db.session.commit()
+                except IntegrityError:
+                        flash("Username or E-mail already exists!")
+                else: 
+                        return redirect(url_for('login'))
+        return render_template('register.html')
 
-@app.route("/populate")
-def populate():
-        user = User(username='marcelo', email= "g@g.com", password_hash='a')
-        post1 = Post(title = "post 1", body= "texto do post", autor= user)
-        post2 = Post(title = "post 2", body= "texto do post 2", autor= user)
-        db.session.add(user)
-        db.session.add(post1)
-        db.session.add(post2)
-        db.session.commit()
+@app.route('/login', methods=["GET", "POST"])
+def login():
+        if current_user.is_authenticated:
+                return redirect(url_for('index'))
+        if request.method == "POST":
+                username = request.form['username']
+                password = request.form['password']
+                user = User.query.filter_by(username=username).first()
+                if user is None or not user.creck_password(password):
+                        flash("Incorrect Username or password")
+                        return redirect(url_for('login'))
+                login_user(user)
+                return redirect(url_for('index'))
+
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+        logout_user()
         return redirect(url_for('index'))
+
